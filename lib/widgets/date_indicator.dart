c4cfb57a9afebff8cfe3cd4beffb2b152b27fb0b
Alex Bogdos <alxbogdos2004@gmail.com>
2022-04-21 13:21:26 +0300

    Updated

--- lib/widgets/date_indicator.dart
+++ lib/widgets/date_indicator.dart
@@ -12,32 +12,36 @@ class DateIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 76,
-      height: 22,
-      alignment: Alignment.topCenter,
-      decoration: BoxDecoration(
-        color: const Color(0xFF736ff1),
-        borderRadius: BorderRadius.circular(15.0),
-        boxShadow: [
-          BoxShadow(
-            color: const Color(0xFF343434).withOpacity(0.1),
-            blurRadius: 6.0,
-            offset: const Offset(6, 6),
-          )
-        ],
-      ),
-      child: Center(
-        child: AutoSizeText(
-          date,
-          maxLines: 1,
-          maxFontSize: 12,
-          textAlign: TextAlign.center,
-          style: GoogleFonts.poppins(
-            color: Colors.white,
+    if (date == "") {
+      return const SizedBox.shrink();
+    } else {
+      return Container(
+        width: 76,
+        height: 22,
+        alignment: Alignment.topCenter,
+        decoration: BoxDecoration(
+          color: const Color(0xFF736ff1),
+          borderRadius: BorderRadius.circular(15.0),
+          boxShadow: [
+            BoxShadow(
+              color: const Color(0xFF343434).withOpacity(0.1),
+              blurRadius: 6.0,
+              offset: const Offset(6, 6),
+            )
+          ],
+        ),
+        child: Center(
+          child: AutoSizeText(
+            date,
+            maxLines: 1,
+            maxFontSize: 12,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.poppins(
+              color: Colors.white,
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
